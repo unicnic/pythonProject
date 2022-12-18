2db45d5a53872103bbb75a988f096f29a3f30410
unicnic <117794274+unicnic@users.noreply.github.com>
2022-12-18 20:52:29 +0500

    Update

--- main.py
+++ main.py
@@ -29,14 +29,3 @@ class Reviewer(Mentor):
     pass
 
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
-
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
-
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-
-print(best_student.grades)
